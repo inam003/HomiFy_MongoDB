53cc8433f416ed977ae80443a9d654c449a574c4
Muhammad Inam Aslam <inam.aslam003@gmail.com>
2025-08-25 10:11:47 +0500

    remove unnecessary file

---
+++ utils/databaseUtil.js
@@ -0,0 +1,27 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(MONGODB_URL)
+    .then((client) => {
+      console.log("Connected to MongoDB");
+      _db = client.db("homify");
+      callback();
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
+      throw error;
+    });
+};
+
+const getDb = () => {
+  if (!_db) {
+    console.log("No database found!");
+  }
+  return _db;
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
